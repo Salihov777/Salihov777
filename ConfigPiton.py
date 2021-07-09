4d50c780aa14662f070af7f2caf501bab8664c76
Salihov777 <73647847+Salihov777@users.noreply.github.com>
2021-07-09 22:40:14 +0300

    Add files via upload

--- ConfigPiton.py
+++ ConfigPiton.py
@@ -1,4 +1,2 @@
-def Token():
-    return '1797905957:AAGiG4ADxEhwRF7J5epQhTHCq6OPlcd2lYs'
-def ApiOWM():
-    return 'd949a4ffb72d5be1919ffe5820f8ecea'
+Token  = '1797905957:AAGiG4ADxEhwRF7J5epQhTHCq6OPlcd2lYs'
+ApiOWM = 'd949a4ffb72d5be1919ffe5820f8ecea'
